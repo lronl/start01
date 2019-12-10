58cab8510b48eba903e1cf99047438825c60a501
Iron <48250777+lronl@users.noreply.github.com>
2019-12-10 23:56:38 +0200

    Update Home_Less2.py

--- Home_Less2.py
+++ Home_Less2.py
@@ -53,3 +53,40 @@ print(my_shop.name, 'Total Sell :', my_sold.quantity + i)
 # Перегрузить один любой унарный метод.
 # Ожидаемый результат: умножаю точку с координатами 1,2,3 на
 # другую точку с такими же координатами, получаю результат 1, 4, 9.
+
+class Point:
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        print('Working...')
+
+    def __add__(self, other):
+        return (self.x + other.x,
+                self.y + other.y,
+                self.z + other.z)
+
+    def __truediv__(self, other):
+        return (self.x / other.x,
+                self.y / other.y,
+                self.z / other.z)
+
+    def __mul__(self, other):
+        return (self.x * other.x,
+                self.y * other.y,
+                self.z * other.z)
+
+    def __sub__(self, other):
+        return (self.x - other.x,
+                self.y - other.y,
+                self.z - other.z)
+
+
+my_fun = Point(1, 2, 3)
+my_fun2 = Point(1, 2, 3)
+
+print(my_fun + my_fun2)
+print(my_fun * my_fun2)
+print(my_fun - my_fun2)
+print(my_fun / my_fun2)
