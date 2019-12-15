f8cc75a6eed02f2530e095366bb0c164c09a1d28
lronl <basov.nikolay.ivanovich@gmail.com>
2019-12-15 18:53:29 +0200

    test

--- Less3/Home_Less3_3.py
+++ Less3/Home_Less3_3.py
@@ -1,7 +1,17 @@
-import math
+# Создать класс комплексного числа и реализовать для него арифметические операции.
+
+from math import *
 #class complex:
 
 x = complex(1,2)
+y = complex(3, 4)
 print(x)
 print(x.real)
 print(x.imag)
+
+print(y)
+print(y.real)
+print(y.imag)
+
+z = x + y
+print(z)
