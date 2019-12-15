19479989c3603d3a9f60c42e0cfe754a54f05c60
lronl <basov.nikolay.ivanovich@gmail.com>
2019-12-15 19:29:58 +0200

    test

--- Less3/Home_Less3_3.py
+++ Less3/Home_Less3_3.py
@@ -1,17 +1,34 @@
 # Создать класс комплексного числа и реализовать для него арифметические операции.
 
-from math import *
-#class complex:
+class ComplexMy:
 
-x = complex(1,2)
-y = complex(3, 4)
-print(x)
-print(x.real)
-print(x.imag)
+    def __init__(self, x, y):
+        self.x = complex(x, y)
+        self.y = complex(x, y)
+        print('Working...')
 
-print(y)
-print(y.real)
-print(y.imag)
+    def __add__(self, other):
+        return (self.x + other.x)
 
-z = x + y
-print(z)
+    def __truediv__(self, other):
+        return (self.x / other.x)
+
+    def __mul__(self, other):
+        return (self.x * other.x)
+
+    def __sub__(self, other):
+        return (self.x - other.x)
+
+
+my_fun = ComplexMy(1, 2)
+my_fun2 = ComplexMy(1, 2)
+
+x1 = my_fun + my_fun2
+x2 = my_fun * my_fun2
+x3 = my_fun - my_fun2
+x4 = my_fun / my_fun2
+
+print(x1, x1.real, x1.imag)
+print(x2, x2.real, x2.imag)
+print(x3, x3.real, x3.imag)
+print(x4, x4.real, x4.imag)
